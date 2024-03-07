b0a4077fdd6322e5beb56c33b11ff34480b901c2
Sridhar-123 <kambalasridhar15@gmail.com>
2024-03-07 22:43:15 +0530

    main

---
+++ recarea.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Write your code here
+    int lenght,breadth,area;
+    cin>>lenght>>breadth;
+    area=lenght*breadth;
+    cout<<area;
+    return 0;
+}
